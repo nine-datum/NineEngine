278d12734b6e34d53ca4ffd2d9e076a891fa5579
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2023-07-04 19:39:27 +0200

    texturing in a right way

--- nine/lwjgl/LWJGL_OpenGL.java
+++ nine/lwjgl/LWJGL_OpenGL.java
@@ -7,6 +7,7 @@ import javax.imageio.ImageIO;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 
 import nine.buffer.Buffer;
@@ -19,10 +20,40 @@ import nine.opengl.ShaderCompiler;
 import nine.opengl.Texture;
 
 import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DataBuffer;
+import java.util.Hashtable;
 
 public class LWJGL_OpenGL implements OpenGL
 {
+    static ColorModel rgbaModel;
+    static ColorModel rgbModel;
+
+    static
+    {
+        rgbaModel = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_sRGB),
+                                            new int[] {8,8,8,8},
+                                            true,
+                                            false,
+                                            ComponentColorModel.TRANSLUCENT,
+                                            DataBuffer.TYPE_BYTE);
+
+        rgbModel = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_sRGB),
+                                            new int[] {8,8,8,0},
+                                            false,
+                                            false,
+                                            ComponentColorModel.OPAQUE,
+                                            DataBuffer.TYPE_BYTE);
+    }
+
     @Override
     public DrawingBuffer vao(Buffer<Integer> elements)
     {
@@ -84,6 +115,35 @@ public class LWJGL_OpenGL implements OpenGL
                 image = new BufferedImage(1,1,BufferedImage.TYPE_INT_RGB);
                 image.setRGB(0, 0, 255);
             }
+
+            BufferedImage actualImage;
+            ColorModel model;
+            int bands;
+
+            if (image.getColorModel().hasAlpha())
+            {
+                bands = 4;
+                model = rgbaModel;
+            }
+            else
+            {
+                bands = 3;
+                model = rgbModel;
+            }
+
+            int width = image.getWidth();
+            int height = image.getHeight();
+
+            WritableRaster raster = Raster.createInterleavedRaster(DataBuffer.TYPE_BYTE, width, height, bands, null);
+            actualImage = new BufferedImage(model,raster,false,new Hashtable<>());
+    
+            Graphics2D g = (Graphics2D)actualImage.getGraphics();
+            g.setColor(new Color(0f,0f,0f,0f));
+            g.fillRect(0, 0, width, height);
+            g.translate(0, height);
+            g.scale(1f, -1f);
+            g.drawImage(image,0,0,null);
+            image = actualImage;
         
             byte[] data = ((DataBufferByte)image.getRaster().getDataBuffer()).getData();
             ByteBuffer buffer = ByteBuffer.allocateDirect(data.length);
@@ -102,7 +162,7 @@ public class LWJGL_OpenGL implements OpenGL
                 image.getWidth(),
                 image.getHeight(),
                 0,
-                image.getColorModel().hasAlpha() ? GL11.GL_RGBA : GL11.GL_RGB,
+                image.getColorModel().hasAlpha() ? GL15.GL_RGBA : GL15.GL_RGB,
                 GL11.GL_UNSIGNED_BYTE,
                 buffer);
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
