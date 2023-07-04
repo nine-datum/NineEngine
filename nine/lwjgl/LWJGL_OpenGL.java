c247c804840f461483119cd35f0e236ad9dab328
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2023-07-04 18:24:45 +0200

    texturing, but with workarounds

--- nine/lwjgl/LWJGL_OpenGL.java
+++ nine/lwjgl/LWJGL_OpenGL.java
@@ -92,6 +92,10 @@ public class LWJGL_OpenGL implements OpenGL
             buffer.flip();
 
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+    		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
+    		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
             GL11.glTexImage2D(GL11.GL_TEXTURE_2D,
                 0,
                 GL11.GL_RGBA,
@@ -107,11 +111,6 @@ public class LWJGL_OpenGL implements OpenGL
 
 		return drawing -> () ->
         {
-            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
-		    GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-    		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
-    		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
-            
             GL13.glActiveTexture(GL13.GL_TEXTURE0);
 		    GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
             drawing.draw();
