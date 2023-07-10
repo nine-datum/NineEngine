8d5b8d67893d706df9364bebda2f1ad6bc1eb5bb
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2023-07-10 11:12:58 +0200

    time fix (no runtime memory allocation)

--- nine/math/Time.java
+++ nine/math/Time.java
@@ -1,17 +1,11 @@
 package nine.math;
 
-import java.time.Duration;
-import java.time.LocalTime;
-import java.time.temporal.Temporal;
-
 public class Time implements ValueFloat
 {
     @Override
     public void accept(FloatAcceptor acceptor)
     {
-        Temporal currentFrame = LocalTime.now();
-        Duration frameTime = Duration.between(LocalTime.MIN, currentFrame);
-
-        acceptor.call((float)(frameTime.getSeconds() + frameTime.getNano() * 0.000000001));
+        long masked = System.currentTimeMillis() % 86_400_000;
+        acceptor.call((masked * 0.001f));
     }
 }
