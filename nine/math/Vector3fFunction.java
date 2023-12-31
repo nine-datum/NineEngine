5984c9bc95605afbcb8c861bfe80f9ef100c9a5c
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2023-12-31 10:02:41 +0100

    wasd input

--- nine/math/Vector3fFunction.java
+++ nine/math/Vector3fFunction.java
@@ -3,4 +3,6 @@ package nine.math;
 public interface Vector3fFunction
 {
     void call(float x, float y, float z, Vector3f.XYZAction acceptor);
+
+    static final Vector3fFunction identity = (x, y, z, action) -> action.call(x, y, z);
 }
