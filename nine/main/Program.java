12f266b871f770db0ee17b81b8889bdce6877468
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2024-01-04 18:31:22 +0100

    working tests

--- nine/main/Program.java
+++ nine/main/Program.java
@@ -1,6 +1,5 @@
 package nine.main;
 
-import jena.lang.value.NoneValue;
 import nine.io.FileStorage;
 
 public class Program
@@ -8,6 +7,6 @@ public class Program
     public static void main(String[] args)
     {
         var storage = new FileStorage();
-        storage.loadScript("resources/scripts/main.jena").call(NoneValue.instance);
+        storage.loadScript("resources/scripts/main.jena");
     }
 }
