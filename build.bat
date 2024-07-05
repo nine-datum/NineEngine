90f4f6a3c8835ea93c8415ab8f6ab8c5a4c362c3
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2024-07-05 15:22:25 +0200

    updated build.bat

--- build.bat
+++ build.bat
@@ -1,14 +1,27 @@
+@echo off
+
 rd /s /q build
 mkdir build
+
+echo build : collecting sources
+
 dir /s /b *.java > build/sourcelist.txt
+
+echo build : compiling classes
+
 javac -target 11 -source  11 -classpath lib/* -d build/src @build/sourcelist.txt
 del build/sourcelist.txt
 cd build
+
+echo build : writing manifest
+
 (echo Main-Class: nine.main.Program & echo Class-Path: . & echo. ) > manifest.txt
+
+echo build : including dependencies...
+
 mkdir lib
 cd lib
 
-@echo off
 setlocal
 
 for %%f in ("../../lib/*") do (
@@ -18,6 +31,10 @@ for %%f in ("../../lib/*") do (
 
 endlocal
 
+echo build : compressing archive...
+
 cd ..
 jar cvfm nine.jar manifest.txt -C src/ . -C lib/ .
 cd ..
+
+echo build : done
